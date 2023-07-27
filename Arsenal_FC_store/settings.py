1174941498bd269891eead248ae335db9d37077d
AdamRalph123 <adambyrne16@gmail.com>
2023-07-27 15:39:18 +0000

    Removed secret key and set debug

--- Arsenal_FC_store/settings.py
+++ Arsenal_FC_store/settings.py
@@ -15,19 +15,20 @@ import os
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['adamralph123-arsenal-fc-store.herokuapp.com', '8000-adamralph12-arsenalfcst-gwe2kdquwux.ws-eu102.gitpod.io', 'localhost']
+ALLOWED_HOSTS = ['arsenal-fc-store.herokuapp.com',
+ '8000-adamralph12-arsenalfcst-gwe2kdquwux.ws-eu102.gitpod.io', 'localhost']
 
 
 # Application definition
