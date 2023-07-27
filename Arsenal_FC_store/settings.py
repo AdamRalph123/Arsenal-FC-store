763f3a5499ff1a684fdd392f5e60fee7c2ea5684
AdamRalph123 <adambyrne16@gmail.com>
2023-07-27 15:57:34 +0000

    Removed secret key and set debug

--- Arsenal_FC_store/settings.py
+++ Arsenal_FC_store/settings.py
@@ -15,7 +15,7 @@ import os
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
