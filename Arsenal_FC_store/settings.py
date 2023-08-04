e10caa645a480bc681fd8899a1614637b0b2f29b
AdamRalph123 <adambyrne16@gmail.com>
2023-08-04 10:32:38 +0000

    Deployment part 1

--- Arsenal_FC_store/settings.py
+++ Arsenal_FC_store/settings.py
@@ -17,17 +17,17 @@ if os.path.exists("env.py"):
     import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-x)vn-w_0e$+h-e5=6m#*d=s4v9%&+%x9bhy)7int(8%jkr*o)('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['Arsenal-FC-store.herokuapp.com', '8000-adamralph12-arsenalfcst-gwe2kdquwux.ws-eu102.gitpod.io', 'localhost']
 
