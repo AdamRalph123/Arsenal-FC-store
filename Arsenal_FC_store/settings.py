34260d98d1a5d0715c13248012b070717af9a74a
AdamRalph123 <adambyrne16@gmail.com>
2023-08-08 13:30:58 +0000

    Set debug to development

--- Arsenal_FC_store/settings.py
+++ Arsenal_FC_store/settings.py
@@ -29,10 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = [ 'arsenal-fc-store-9dd3251c1b46.herokuapp.com', 'localhost']
-
+ALLOWED_HOSTS = [ 'arsenal-fc-store-9dd3251c1b46.herokuapp.com' ,'localhost']
 
 # Application definition
 
