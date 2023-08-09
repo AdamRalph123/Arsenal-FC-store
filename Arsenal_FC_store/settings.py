88e9250b3d32fba387aa5069f2f5111af532fbbd
AdamRalph123 <adambyrne16@gmail.com>
2023-08-09 10:58:53 +0000

    base.css and login/reg refactor

--- Arsenal_FC_store/settings.py
+++ Arsenal_FC_store/settings.py
@@ -29,10 +29,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
-ALLOWED_HOSTS = [ 'arsenal-fc-store-9dd3251c1b46.herokuapp.com' ,'localhost']
+ALLOWED_HOSTS = [ '8000-adamralph12-arsenalfcst-gwe2kdquwux.ws-eu102.gitpod.io','localhost']
 
+'arsenal-fc-store-9dd3251c1b46.herokuapp.com' 
 # Application definition
 
 INSTALLED_APPS = [
