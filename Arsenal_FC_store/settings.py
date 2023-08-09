1c1d6a6019840e2dbfff5bec925fc2476d46c7fd
AdamRalph123 <adambyrne16@gmail.com>
2023-08-09 11:47:03 +0000

    Added error 404 and 500 html page

--- Arsenal_FC_store/settings.py
+++ Arsenal_FC_store/settings.py
@@ -29,11 +29,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = [ '8000-adamralph12-arsenalfcst-gwe2kdquwux.ws-eu102.gitpod.io','localhost']
+ALLOWED_HOSTS = ['arsenal-fc-store-9dd3251c1b46.herokuapp.com' ,'localhost']
 
-'arsenal-fc-store-9dd3251c1b46.herokuapp.com' 
 # Application definition
 
 INSTALLED_APPS = [
