d4ee9744dcc0a8ad53655e4649d874e5befa7414
AdamRalph123 <adambyrne16@gmail.com>
2023-08-14 15:02:19 +0000

    Changed contact map size to allow full screen

--- Arsenal_FC_store/settings.py
+++ Arsenal_FC_store/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['arsenal-fc-store-9dd3251c1b46.herokuapp.com','localhost']
+ALLOWED_HOSTS = ['arsenal-fc-store-9dd3251c1b46.herokuapp.com' ,'localhost']
 
 # Application definition
 
